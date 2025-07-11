dea62f09ae9eef7c7eda84bfb90d94e1d2d5b16c
unknown <your_email@example.com>
2025-07-12 00:33:11 +0700

    Initial commit

--- client/src/api.js
+++ client/src/api.js
@@ -2,16 +2,13 @@ import axios from "axios";
 
 // Create a new axios instance
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Your backend API URL
+  baseURL: "http://localhost:5000/api",
 });
 
-// This is an "interceptor" that runs before every single request is sent.
 api.interceptors.request.use(
   (config) => {
-    // Get the token from localStorage
     const token = localStorage.getItem("token");
 
-    // If a token exists, add it to the Authorization header
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
